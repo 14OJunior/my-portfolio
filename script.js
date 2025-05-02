338fa16006aab776e0ba2ed8a674a65330c026eb
Ornildo Junior <101369871+14OJunior@users.noreply.github.com>
2025-05-02 13:55:30 -0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function toggleMenu() {
-    const menuList = document.querySelector('.menu-list');
-    menuList.classList.toggle('active'); // Adiciona ou remove a classe 'active' para mostrar/ocultar o menu
-}
