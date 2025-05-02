8ff70df3c3e43cca8f84e04b273a1e9bd417443f
Ornildo Junior <101369871+14OJunior@users.noreply.github.com>
2025-05-02 13:56:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    const menuList = document.querySelector('.menu-list');
+    menuList.classList.toggle('active'); // Adiciona ou remove a classe 'active' para mostrar/ocultar o menu
+}
